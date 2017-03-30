0869c54e63fde7db0ca7755760592f33f436dad5
Megan Sullivan <meganesulli@gmail.com>
2017-03-30 01:07:00 -0400

    Finish project 4: Mult and Fill

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,54 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(LOOP)
+  // Initialize screen word
+  @SCREEN
+  D=A
+  @addr
+  M=D // addr = SCREEN
+
+  // Get input from keyboard
+  @KBD
+  D=M
+  @FILL
+  D; JNE
+
+(CLEAR)
+  @KBD
+  D=A
+  @addr
+  D=D-M // D = KBD - addr
+  @STOP
+  D; JEQ // if addr == KBD (addr is past screen map), goto STOP
+
+  @addr
+  A=M // set A to address stored in addr
+  M=0 // RAM[addr] = 0
+
+  @addr
+  M=M+1 // addr = addr + 1
+  @CLEAR
+  0; JMP
+
+(FILL)
+  @KBD
+  D=A
+  @addr
+  D=D-M // D = KBD - addr
+  @STOP
+  D; JEQ // if addr == KBD (addr is past screen map), goto STOP
+
+  @addr
+  A=M // set A to address stored in addr
+  M=-1 // RAM[addr] = -1
+
+  @addr
+  M=M+1 // addr = addr + 1
+  @FILL
+  0; JMP
+
+(STOP)
+  @LOOP
+  0; JMP
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,31 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+  @R2
+  M=0 // R2=0
+  @i
+  M=0 // i=0
+
+(LOOP)
+  @R0
+  D=M
+  @i
+  D=D-M
+  @END
+  D; JEQ // if R0-i == 0, goto END
+
+  @R1
+  D=M
+  @R2
+  M=D+M // R2 = R1 + R2
+
+  @i
+  M=M+1 // i = i+1
+  @LOOP
+  0; JMP
+
+(END)
+  @END
+  0; JMP
+
